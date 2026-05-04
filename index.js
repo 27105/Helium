961ad1da55d997bb08c4dfaa69db4cf78b0869d4
edgoad <rowencbb@gmail.com>
2026-05-03 20:28:51 -0500

    Changed error message

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function calculate(){
         display.value = eval(display.value);
     }
     catch(error){
-        display.value = "nope.";
+        display.value = "ERROR: Operation symbol was in last";
 		let audio = new Audio('./sfx/combobreak.mp3');
 		audio.play();
     }
